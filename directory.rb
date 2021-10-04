b25e994202b37a8fea26c753010746ffa668e039
Matt Zimmer <zimmermja@gmail.com>
2021-10-04 15:06:43 +0000

    directory.rb: methods added

--- directory.rb
+++ directory.rb
@@ -1,7 +1,18 @@
 students = ["Dr. Hannibal Lecter","Darth Vader","Nurse Ratched","Michael Corleone","Alex DeLarge","The Wicked Witch of the West",
 "Terminator","Freddy Krueger","The Joker","Joffrey Baratheon","Norman Bates","Sauron"]
-# Begin by printing the fill list of students
-puts "The students of Villains Academy\n-------------"
-students.each { |stud| puts stud }
-# Finish by printining the total numbers of students
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy\n-------------"   
+end
+
+def print_names(students)
+  students.each { |stud| puts stud }    
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"  
+end
+
+print_header
+print_names(students)
+print_footer(students)
