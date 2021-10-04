c3d9799292be0a6ed448d57aca1840d93a115467
Matt Zimmer <zimmermja@gmail.com>
2021-10-04 15:30:14 +0000

    directory.rb: changed student list to input students

--- directory.rb
+++ directory.rb
@@ -1,4 +1,34 @@
-students = [
+def print_header
+  puts "The students of Villains Academy\n-------------"   
+end
+
+def print_names(students)
+  students.each { |s| puts s }    
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"  
+end
+
+def input_students
+  puts "Enter student names into the directory here"
+  puts "Hit return twice to finish"
+  students = []
+  loop do
+    input = gets.chomp
+    break if input.empty?
+    students << input
+    puts "There #{(sc = students.count) == 1? "is" : "are"} #{sc} student#{sc == 1? "" : "s"} in the directory."
+  end
+  students
+end
+
+students = input_students
+print_header
+print_names(students)
+print_footer(students)
+
+=begin
     {:name => "Dr. Hannibal Lecter", :cohort => :october},
     {:name => "Darth Vader", :cohort => :october},
     {:name => "Nurse Ratched", :cohort => :october},
@@ -11,19 +41,4 @@ students = [
     {:name => "Joffrey Baratheon", :cohort => :october},
     {:name => "Norman Bates", :cohort => :october},
     {:name => "Sauron", :cohort => :october}]
-
-def print_header
-  puts "The students of Villains Academy\n-------------"   
-end
-
-def print_names(students)
-  students.each { |s| puts "#{s[:name]} (#{s[:cohort].capitalize} cohort)" }    
-end
-
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"  
-end
-
-print_header
-print_names(students)
-print_footer(students)
+=end
