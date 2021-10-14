e71f254eec954e601b0394bbfe4bb904b611d032
Matt Zimmer <zimmermja@gmail.com>
2021-10-14 14:18:29 +0100

    Changes to viewing methods: menu list reordered with viewing at top; print_footer and directory_count_statement local variables replaced with global; default params for print_names; white space labels added for menu, generic messages and viewing, methods organised; notes added

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
 =begin
-- Notes added
-- Functions renamed to be more descriptive
-- Remove option to reset directory (not required)
-- Error message added when an invalid choice is made from the IM
-- Remade the IM loop
+- List re-ordered by usefulness
+- print_footer and directory_count_statement: local variable replaced with global
+- print_names default paratmeters set; notes added
+- labels for the menu, generic messages and viewing students added; methods reorganised accordingly
+- notes added to viewing
 =end
 
 $students = []
 
+#----------------------------------------
+# INTERACTIVE MENU 
+#----------------------------------------
 def interactive_menu # This menu will continue to loop until a blank choice is selected.
   im_show_options
   im_chose_something = im_choice(STDIN.gets.chomp.upcase)
@@ -16,9 +19,9 @@ end
 
 def im_show_options
   print_header("directory menu")
-  puts " (A) Add students to directory
+  puts " (V) View list of students
+ (A) Add students to directory
  (D) Delete students from directory
- (V) View list of students
  (E) Export list
  (I) Import list
  (S) Source code
@@ -39,44 +42,53 @@ def im_choice(choice)
   return true # Returns true to signal that a choice was made
 end
 
+#----------------------------------------
+# GENERIC MESSAGES
+#----------------------------------------
+
 def invalid_input(to_do)
   puts "\nERROR: INVALID INPUT. #{to_do}.\n "
-end
-
-def view_students
-  puts "attempt print"
-  print_header("list of students")
-  print_names($students, "", false, true)
-  print_footer($students)  
 end
 
 def print_header(header)
   puts "#{dashes = "-"*40}\n#{header.upcase}\n#{dashes}"   
 end
 
-def print_footer(students)
-  puts "#{directory_count_statement(students)}\nPress ENTER to return to menu"; STDIN.gets
+def print_footer
+  puts "#{directory_count_statement}\nPress ENTER to return to menu"
+  STDIN.gets # This is here so the user has to press ENTER to proceed; gives user a moment to achknowledge this statement before proceeding
 end
 
-def directory_count_statement(students) # e.g. "There is 1 student in the directory.""
-  "There #{(sc = students.count) == 1? "is" : "are"} #{sc} student#{sc == 1? "" : "s"} in the directory."
+def directory_count_statement # e.g. "There is 1 student in the directory.""
+  "There #{(sc = $students.count) == 1? "is" : "are"} #{sc} student#{sc == 1? "" : "s"} in the directory."
 end
 
-def print_names(students, s_char, u_twelve, by_cohort)
-  students.select! { |s| s[:name].chr.downcase == s_char.downcase } if s_char != ""
-  students.select! { |s| s[:name].size < 12 } if u_twelve  
+#----------------------------------------
+# VIEWING STUDENTS
+#----------------------------------------
+
+def view_students
+  print_header("list of students")
+  print_names($students)
+  print_footer
+end
+
+def print_names(students, by_cohort=true, s_char="", u_twelve=false)
+  students.select! { |s| s[:name].chr.downcase == s_char.downcase } if s_char != "" # If s_char is specified, proceeds only with students whose name begins with s_char
+  students.select! { |s| s[:name].size < 12 } if u_twelve # If u_twelve is true, proceeds only with students whose names are less that 12 characters
   by_cohort ? print_names_by_cohort(students) : print_names_by_list(students)
 end
 
 def print_names_by_cohort(students)
-  (students.map { |s| s[:cohort] }.uniq).each do |cohort|
+  cohorts = students.map { |s| s[:cohort] }.uniq # Returns an array listing all the unique cohorts
+  cohorts.each do |cohort|
     puts "#{cohort.to_s} cohort:"
-    students.select { |s| s[:cohort] == cohort }.each_with_index { |x, i| puts "  #{i+1}. #{x[:name]}" }
+    students.select { |s| s[:cohort] == cohort }.each_with_index { |x, i| puts "  #{i+1}. #{x[:name]}" } # For each cohort, selects students in that cohort, and puts a list of them
   end
 end
 
 def print_names_by_list(students)
-  (students.map { |s| "#{s[:name]}, cohort: #{s[:cohort].capitalize}. Hobby: #{s[:hobby]}" }).sort.each_with_index { |st, i| puts "  #{i+1}. #{st}" }
+  (students.map { |s| "#{s[:name]}, cohort: #{s[:cohort]}. Hobby: #{s[:hobby]}" }).sort.each_with_index { |st, i| puts "  #{i+1}. #{st}" } # An alphabetical list of students, with cohort and hobbies listed
 end
 
 def delete_students
