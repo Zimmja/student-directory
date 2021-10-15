f549bbc0998ab15dc89ad8a77509bc4a80216c0c
Matt Zimmer <zimmermja@gmail.com>
2021-10-15 12:30:28 +0100

    Export updated; handling of cohort lists updated

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+# Export format updated to be consistent with import
+# Refactored cohort listing
+
 $students = []
 $cohorts = []
 
@@ -16,8 +19,8 @@ def im_show_options
  (C) Add cohort to directory
  (A) Add students to cohort
  (D) Delete students from directory
- (E) Export list
- (I) Import list
+ (E) Export cohort
+ (I) Import cohort
  (S) Source code
  Press ENTER without choosing an option to exit"   
 end
@@ -28,7 +31,7 @@ def im_choice(choice)
     when "C" then choice_add_cohort
     when "D" then choice_delete_students
     when "V" then choice_view_students
-    when "E" then export_list
+    when "E" then choice_export
     when "I" then import_list
     when "S" then view_source
     when "" then return false
@@ -76,13 +79,16 @@ def print_names(students, by_cohort=true, s_char="", u_twelve=false)
 end
 
 def print_names_by_cohort(students)
+  puts directory_count_statement
   update_cohorts
   $cohorts.each do |cohort|
     puts "#{cohort.to_s} cohort:"
-    cohort_students = students.select { |s| s[:cohort] == cohort } # Creates a list of students in the cohort
+    cohort_students = get_cohort_students(students, cohort)
     cohort_students.count == 0 ? (puts "  This cohort is empty") : cohort_students.each_with_index { |x, i| puts "  #{i+1}. #{x[:name]}" }
   end
 end
+
+def get_cohort_students(students, cohort) students.select { |s| s[:cohort] == cohort } end 
 
 def print_names_by_list(students)
   (students.map { |s| "#{s[:name]}, cohort: #{s[:cohort]}. Hobby: #{s[:hobby]}" }).sort.each_with_index { |st, i| puts "  #{i+1}. #{st}" } # An alphabetical list of students, with cohort and hobbies listed
@@ -131,15 +137,19 @@ end
 
 def choice_add_cohort
   print_header("add cohort to directory")
+  handle_cohort_choice("Enter a new cohort below") {check_cohort_choice(STDIN.gets.chomp)}
+end
+
+def handle_cohort_choice(prompt)
   loop do
-    list_cohorts
-    puts "Enter a new cohort below\nOR leave blank and press ENTER to return to menu"
-    break if check_cohort_choice(STDIN.gets.chomp) == false # Call check_cohort_choice, and breaks this loop if the method returns false (i.e. no new cohorts added)
+    list_cohorts # Prints a numbered list of cohorts
+    puts "#{prompt}\nOR leave blank and press ENTER to return to menu" # Prompts the user for an input
+    break if yield == false # The yield is defined in the calling method; in each case, it asks the user for an input and handles the input, returning false if no choice is made
   end
 end
 
 def list_cohorts
-  puts "Cohorts:"
+  puts "COHORTS:"
   update_cohorts # Double-check the cohorts are up to date
   $cohorts.each_with_index {|c, i| puts "  #{i+1}. #{ c}"}
 end
@@ -161,12 +171,7 @@ end
 
 def choice_add_students
   print_header("add students to cohort")
-  puts directory_count_statement
-  loop do
-    list_cohorts
-    puts "Choose which cohort number to add a student to\nOR leave blank and press ENTER to return to menu"
-    break if check_add_choice(STDIN.gets.chomp, $cohorts.count) == false # Call check_add_choice, and breaks this loop if the method returns false (i.e. no cohort chosen)
-  end
+  handle_cohort_choice("Choose which cohort to add a student to") {check_add_choice(STDIN.gets.chomp, $cohorts.count)}
 end
 
 def check_add_choice(choice, c_count)
@@ -199,16 +204,42 @@ def input_student_details(detail, default=nil)
 end
 
 #----------------------------------------
+# EXPORT COHORT
+#----------------------------------------
 
-
-
-def export_list
-  print "Export name: "
-  export_file = File.open("#{name = STDIN.gets.chomp}.csv", "w")
-  $students.each { |student| export_file.puts "#{student[:name]}, #{student[:cohort]}, #{student[:hobby]}" }
-  export_file.close
-  puts "File exported: #{name}.csv\n "
+def choice_export
+  print_header("export cohort")
+  handle_cohort_choice("Choose which cohort to export") {check_export_choice(STDIN.gets.chomp, $cohorts.count)}
 end
+
+def check_export_choice(choice, c_count)
+  return false if choice == "" 
+  choice_i = choice.to_i - 1 # e.g. If choosing "1.", the index we'll need to delete is 0
+  valid_choice = (0...c_count).include?(choice_i) # True if the chosen number is between 1 and the number of cohorts in the directory
+  valid_choice ? export_cohort(choice_i) : invalid_input("Please choose a number between 1 and #{c_count}")
+end
+
+def export_cohort(i)
+  chosen_cohort = $cohorts[i].to_s.downcase
+  delete_file = false
+
+  export_file = File.open("cohort_#{chosen_cohort}.csv", "w")
+    export_file.puts chosen_cohort
+    cohort_students = get_cohort_students($students, $cohorts[i])
+    if cohort_students.count == 0
+      puts "\nEXPORT CANCELLED: cannot export an empty cohort\n "
+      delete_file = true
+    else
+      cohort_students.each{ |s| export_file.puts "#{s[:name]},#{s[:hobby]}," }
+      puts "\nEXPORT SUCCESSFULL: cohort_#{chosen_cohort}.csv\n "
+    end
+  export_file.close
+  File.delete("cohort_#{chosen_cohort}.csv") if delete_file
+end
+
+
+
+
 
 def try_startup_import(is_csv=false)
   filename = (ARGV[0].nil? ? "cohort_hp.csv" : ARGV[0]) # default added
